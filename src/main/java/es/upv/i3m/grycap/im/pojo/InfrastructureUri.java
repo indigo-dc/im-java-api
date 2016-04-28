f16c33540c65b447ca6710e6071e699b4de77067
Alfonso <alpegon3@upvnet.upv.es>
2016-04-28 13:08:55 +0200

    Added unitray tests for the POJOs

--- src/main/java/es/upv/i3m/grycap/im/pojo/InfrastructureUri.java
+++ src/main/java/es/upv/i3m/grycap/im/pojo/InfrastructureUri.java
@@ -3,6 +3,8 @@ package es.upv.i3m.grycap.im.pojo;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import es.upv.i3m.grycap.im.exceptions.InfrastructureUuidNotFoundException;
+
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
@@ -15,8 +17,8 @@ public class InfrastructureUri {
 
   private final String uri;
   private String infrastructureId;
-  private static final String URI_PATTERN = "([^/]+)";
-  private static final int GROUP_WITH_ID_INFO = 4;
+  private static final String UUID_PATTERN =
+      "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}";
 
   public InfrastructureUri(@JsonProperty("uri") String uri) {
     this.uri = uri;
@@ -29,23 +31,22 @@ public class InfrastructureUri {
   /**
    * Returns the infrastructure Id extracted from the infrastructure uri.
    */
-  public String getInfrastructureId() {
+  public String getInfrastructureId()
+      throws InfrastructureUuidNotFoundException {
     if (infrastructureId == null) {
       infrastructureId = extractInfrastructureId();
     }
     return infrastructureId;
   }
 
-  private String extractInfrastructureId() {
-    Pattern ptrn = Pattern.compile(URI_PATTERN);
+  private String extractInfrastructureId()
+      throws InfrastructureUuidNotFoundException {
+    Pattern ptrn = Pattern.compile(UUID_PATTERN);
     Matcher matcher = ptrn.matcher(uri);
-    // The fourth match has the id info
-    // Not using reverse search because the string can have more content
-    // afterwards
-    for (int i = 0; i < GROUP_WITH_ID_INFO; i++) {
-      matcher.find();
+    if (matcher.find()) {
+      return matcher.group(0);
     }
-    return matcher.group(0);
+    throw new InfrastructureUuidNotFoundException();
   }
 
   @Override
